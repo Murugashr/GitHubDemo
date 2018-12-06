ed0edfd8d2b7c7228584774b95e08fa82fefb5d0
Murugan <murugan.v@dsrc.co.in>
2018-12-06 15:33:18 +0530

    New Comment added in th main application

--- spring-cloud-security/src/main/java/com/spring/cloud/security/SpringCloudSecurityApplication.java
+++ spring-cloud-security/src/main/java/com/spring/cloud/security/SpringCloudSecurityApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
+/**
+ * @author murugan.v
+ * This is main application of spring
+ * security cloud project
+ */
 @SpringBootApplication
 //@EnableOAuth2Sso
 public class SpringCloudSecurityApplication {
